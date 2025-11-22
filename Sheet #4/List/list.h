668c9f185592838ff3d1b55379061f7ed12537a6
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-22 12:07:35 +0200

    Update list.h

--- Sheet #4/List/list.h
+++ Sheet #4/List/list.h
@@ -1,1 +1,27 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
 
+#define MAX_SIZE 10
+
+typedef int type;
+
+typedef struct{
+    type arr[MAX_SIZE];
+    int size;
+}List;
+
+void createList(List* l);
+
+void insert(List* l, type element, int index);
+
+type retrive(List* l, int index);
+
+int isEmpty(List* l);
+
+int size(List* l);
+
+void destroy(List* l);
+
+void printList(List* l);
+
+#endif // LIST_H_INCLUDED
