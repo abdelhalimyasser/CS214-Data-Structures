27450424fdcc72187a9dc30bb515dabd5b9c19a1
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-22 13:42:14 +0200

    Update main.c

--- Sheet #4/List/main.c
+++ Sheet #4/List/main.c
@@ -4,8 +4,25 @@
 int main()
 {
     List* list;
-    createList(l);
-    
+    createList(list);
+
+    insert(list, 10, 0);
+    insert(list, 11, 1);
+    insert(list, 12, 2);
+    insert(list, 13, 3);
+    insert(list, 14, 2);
+
+    printList(list);
+
+    printf("The Retrieved: %d\n", retrieve(list, 2));
+
+    printList(list);
+
+    printf("Size: %d\n", size(list));
+
+    destroy(list);
+
+    printf("Is Empty: %d\n", isEmpty(list));
+
     return 0;
 }
-
