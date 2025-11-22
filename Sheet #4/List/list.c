0f99bccbf7df2b9dc98975a062e2db44b844ee16
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-11-22 12:08:23 +0200

    Update list.c

--- Sheet #4/List/list.c
+++ Sheet #4/List/list.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include "list.h"
 
+void createList(List* l) {
+	l->size = 0;
+}
+
+void insert(List* l, type element, int index) {
+}
+
+type retrive(List* l, int index) {
+}
+
+int isEmpty(List* l) {
+	return l->size <= 0;
+}
+
+int size(List* l) {
+	return l->size;
+}
+
+void destroy(List* l) {
+	l->size = 0;
+}
+
+void printList(List* l) {
+    if(isEmpty(l))
+        printf("List is Empty\n");
+        
+	for (int i = 0; i < l->size; i++)
+		printf("%d ", l->data[i]);
+		
+	printf("\n");
+}
